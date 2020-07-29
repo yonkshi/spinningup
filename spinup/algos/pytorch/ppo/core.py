722569900606cdc85708dda349d4067f644d8c87
Yonk Shi <pyshi@kth.se>
2020-07-29 16:26:16 +0200

    Added pointnet backbone to PPO

--- spinup/algos/pytorch/ppo/core.py
+++ spinup/algos/pytorch/ppo/core.py
@@ -6,6 +6,7 @@ import torch
 import torch.nn as nn
 from torch.distributions.normal import Normal
 from torch.distributions.categorical import Categorical
+from spinup.algos.pytorch.common import PointNetfeat
 
 
 def combined_shape(length, shape=None):
@@ -47,7 +48,7 @@ def discount_cumsum(x, discount):
 class Actor(nn.Module):
 
     def _distribution(self, obs):
-        raise NotImplementedError
+        raise NotImplementedErrorMLPCritic
 
     def _log_prob_from_distribution(self, pi, act):
         raise NotImplementedError
@@ -77,7 +78,7 @@ class MLPCategoricalActor(Actor):
         return pi.log_prob(act)
 
 
-class MLPGaussianActor(Actor):
+class PointNetGaussianActor(Actor):
 
     def __init__(self, obs_dim, act_dim, hidden_sizes, activation):
         super().__init__()
@@ -94,7 +95,7 @@ class MLPGaussianActor(Actor):
         return pi.log_prob(act).sum(axis=-1)    # Last axis sum needed for Torch Normal distribution
 
 
-class MLPCritic(nn.Module):
+class PointNetCritic(nn.Module):
 
     def __init__(self, obs_dim, hidden_sizes, activation):
         super().__init__()
@@ -103,13 +104,66 @@ class MLPCritic(nn.Module):
     def forward(self, obs):
         return torch.squeeze(self.v_net(obs), -1) # Critical to ensure v has right shape.
 
-
-
-class MLPActorCritic(nn.Module):
+class PointNetActorCritic(nn.Module):
 
 
     def __init__(self, observation_space, action_space, 
                  hidden_sizes=(64,64), activation=nn.Tanh):
+        super().__init__()
+
+
+        self.feat = PointNetfeat()
+        feat_dim = 1024
+        # policy builder depends on action space
+        if isinstance(action_space, Box):
+            self.pi = PointNetGaussianActor(feat_dim, action_space.shape[0], hidden_sizes, activation)
+        elif isinstance(action_space, Discrete):
+            self.pi = MLPCategoricalActor(feat_dim, action_space.n, hidden_sizes, activation)
+
+        # build value function
+        self.v  = PointNetCritic(feat_dim, hidden_sizes, activation)
+
+    def step(self, obs):
+        with torch.no_grad():
+            feat = self.feat(obs)
+            pi = self.pi._distribution(feat)
+            a = pi.sample()
+            logp_a = self.pi._log_prob_from_distribution(pi, a)
+            v = self.v(feat)
+        return a.numpy(), v.numpy(), logp_a.numpy()
+
+    def act(self, obs):
+        return self.step(obs)[0]
+
+class MLPGaussianActor(Actor):
+
+    def __init__(self, obs_dim, act_dim, hidden_sizes, activation):
+        super().__init__()
+        log_std = -0.5 * np.ones(act_dim, dtype=np.float32)
+        self.log_std = torch.nn.Parameter(torch.as_tensor(log_std))
+        self.mu_net = mlp([obs_dim] + list(hidden_sizes) + [act_dim], activation)
+
+    def _distribution(self, obs):
+        mu = self.mu_net(obs)
+        std = torch.exp(self.log_std)
+        return Normal(mu, std)
+
+    def _log_prob_from_distribution(self, pi, act):
+        return pi.log_prob(act).sum(axis=-1)  # Last axis sum needed for Torch Normal distribution
+
+class MLPCritic(nn.Module):
+
+    def __init__(self, obs_dim, hidden_sizes, activation):
+        super().__init__()
+        self.v_net = mlp([obs_dim] + list(hidden_sizes) + [1], activation)
+
+    def forward(self, obs):
+        return torch.squeeze(self.v_net(obs), -1)  # Critical to ensure v has right shape.
+
+class MLPActorCritic(nn.Module):
+
+    def __init__(self, observation_space, action_space,
+                 hidden_sizes=(64, 64), activation=nn.Tanh):
         super().__init__()
 
         obs_dim = observation_space.shape[0]
@@ -121,7 +175,7 @@ class MLPActorCritic(nn.Module):
             self.pi = MLPCategoricalActor(obs_dim, action_space.n, hidden_sizes, activation)
 
         # build value function
-        self.v  = MLPCritic(obs_dim, hidden_sizes, activation)
+        self.v = MLPCritic(obs_dim, hidden_sizes, activation)
 
     def step(self, obs):
         with torch.no_grad():
